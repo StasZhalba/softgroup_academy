cc0e8e40e729b1bec83704534c087382288013e2
Stas Jalba <stasjalba@mail.ru>
2017-01-27 19:10:14 +0200

    task 2: function "add_author" to database

--- task_2/models/author.php
+++ task_2/models/author.php
@@ -26,6 +26,20 @@ ORDER BY author_surname, author_name ASC ;')){
     return $authors;
 }
 
-function add_author_to_db(){
+function add_author_to_db($surname, $name, $year_of_birth, $year_of_death, $country){
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    $mysqli->set_charset("utf8");
+    if (mysqli_connect_errno()){
+        printf("Неможливо підключитись до бази даних. Код помилки: %s\n", mysqli_connect_error());
+        exit;
+    }
+    $surname = mysqli_real_escape_string($mysqli, trim($surname));
+    $name = mysqli_real_escape_string($mysqli, trim($name));
+    $country = mysqli_real_escape_string($mysqli, trim($country));
 
+    if ($year_of_death == null | empty($year_of_death)) {
+        $mysqli->query("INSERT INTO author (author_surname, author_name, author_year_of_birth, author_death, 
+                          author_country) VALUES ('$surname', '$name', '$year_of_birth', '$year_of_death', '$country');");
+    }
+    $mysqli->close();
 }
