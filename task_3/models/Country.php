3ff0a6c6f7178700a2444c4671b9a05f45719eb4
Stas Jalba <stasjalba@mail.ru>
2017-02-10 09:53:25 +0200

    task 3: class Country

---
+++ task_3/models/Country.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stas Jalba
+ * Date: 10.02.2017
+ * Time: 9:51
+ */
+
+class Country{
+
+    public function city_all(){
+        $mysql = new mysqli($this->host, $this->user, $this->password, $this->dbName);
+
+        if ($result = $mysql->query("SELECT city.city_id, city.city_name, country.country_name FROM city 
+                                        INNER JOIN country ON city.city_country=country.country_id;")){
+            $cities = array();
+            while ($row = mysqli_fetch_assoc($result)){
+                $cities[] = $row;
+            }
+        }
+
+        $mysql->close();
+
+        return $cities;
+    }
+}
